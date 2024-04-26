84938aebd1596acde557339d8c83d81f40292bbb
afnaan <167874750+Afnaan29@users.noreply.github.com>
2024-04-26 23:32:09 +0530

    some php basic's

---
+++ Back-End/php/index.php
@@ -0,0 +1,30 @@
+<?php
+ echo "hello world <br>";
+
+
+ $name = "Afnaan";
+ $age = 17;
+
+ $gpa = 2.5;
+ $pass = true;
+ $salary = 1000;
+ $expences = 500;
+
+
+ echo "My name is {$name}, age {$age} and my gpa is {$gpa} pass: {$pass} <br>";
+ echo "My salary is \${$salary} and expences is \${$expences}<br>";
+ $left_over = $salary - $expences;
+
+ echo "The money i have left over is \${$left_over}"
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php practice</title>
+</head>
+<body>
+    
+</body>
+</html>
