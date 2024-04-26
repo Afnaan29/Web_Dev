53b28508d24a0863c49b6ec95504505d12fc2401
afnaan <167874750+Afnaan29@users.noreply.github.com>
2024-04-26 23:50:10 +0530

    operators

--- Back-End/php/index.php
+++ Back-End/php/index.php
@@ -15,14 +15,59 @@
  echo "My salary is \${$salary} and expences is \${$expences}<br>";
  $left_over = $salary - $expences;
 
- echo "The money i have left over is \${$left_over}"
+ echo "The money i have left over is \${$left_over} <br>";
+
+ $add = $salary + $expences;
+ echo "add operator {$add} <br>";
+
+ $sub = $salary - $expences;
+ echo "sub operator {$sub} <br>";
+
+ $mul = $salary * $expences;
+ echo "mul operator {$mul} <br>";
+
+ $div = $salary / $expences;
+ echo "div operator {$div} <br>";
+
+ $rem = $salary % $expences;
+ echo "div operator {$rem} <br>";
+
+ $power = $salary ** 2;
+ echo "div operator {$power} <br>";
+
+ echo "before increment operator \${$expences} <br> ";
+ $expences++;
+ echo "after increment operator \${$expences} <br>";
+
+ echo "before decrement operator \${$expences} <br>";
+ $expences--;
+ echo "after decrement operator \${$expences} <br>";
+
+/* operator precedence
+    ()
+    **
+    * / %
+    + -
+*/
+    $operator_precedence = ((((5+5)*5)-10)**2);
+    echo $operator_precedence;
 ?>
+
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>php practice</title>
+
+    <style>
+        body{
+            background: #E1E1D9;
+        }
+    </style>
 </head>
 <body>
     
