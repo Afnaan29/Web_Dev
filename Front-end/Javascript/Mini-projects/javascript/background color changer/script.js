2871ad5daf9b4d0035c441e9250d6560dfa8e226
afnaan <167874750+Afnaan29@users.noreply.github.com>
2024-04-29 23:52:45 +0530

    some javascript practice

---
+++ Front-end/Javascript/Mini-projects/javascript/background color changer/script.js
@@ -0,0 +1,13 @@
+let x = document.getElementById("color");
+let i = 0;
+
+let changeColor = () => {
+    let colours = ["brown","blue","green","purple","cyan","yellow"]
+
+    x.style.background = colours[i++]
+    document.getElementById("color_num").innerHTML = i + "/" + colours.length;
+
+    if(i > colours.length - 1){
+        i = 0;
+    }
+}
---
+++ Front-end/Javascript/Mini-projects/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./javascript/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        body: ["Oswald"]
+      },
+    },
+  },
+  plugins: [],
+}
