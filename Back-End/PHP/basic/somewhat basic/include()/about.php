b0c0b11ff1cc5a1c87e451797819a10b52e062ad
afnaan <167874750+Afnaan29@users.noreply.github.com>
2024-05-04 23:20:15 +0530

    include() in php

---
+++ Back-End/PHP/basic/somewhat basic/include()/about.php
@@ -0,0 +1,25 @@
+<?php
+
+include("header.html");
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About</title>
+</head>
+<body>
+    <p>This is website's About page</p>
+    <p>Website's information will be uploaded here</p>
+</body>
+</html>
+
+<?php
+
+include("footer.html");
+
+?>
---
+++ Back-End/PHP/basic/somewhat basic/include()/location.php
@@ -0,0 +1,25 @@
+<?php
+
+include("header.html");
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Locations</title>
+</head>
+<body>
+    <p>This is website's location page</p>
+    <p>location's will be uploaded here</p>
+</body>
+</html>
+
+<?php
+
+include("footer.html");
+
+?>
